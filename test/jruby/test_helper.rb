4200a8c2feaa09fd4c91a66027b35dba9b526831
kares <self@kares.org>
2017-07-05 21:28:46 +0200

    Revert "adjust TypeError's conversion failed error message to match MRI closer"
    This reverts commit ce2a4c1bf2d423d9f0854e1465171a2bd105ce5e.

--- test/jruby/test_helper.rb
+++ test/jruby/test_helper.rb
@@ -46,7 +46,7 @@ module TestHelper
 
   IBM_JVM = RbConfig::CONFIG['host_vendor'] =~ /IBM Corporation/
 
-  JAVA_8 = ENV_JAVA['java.specification.version'] >= '1.8' rescue nil
+  JAVA_8 = ENV_JAVA['java.specification.version'] >= '1.8'
 
   def q
     WINDOWS ? '"' : '\''
