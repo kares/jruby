7300052b2870598009291f3a0677d71862d9f13a
kares <self@kares.org>
2017-08-09 08:43:02 +0200

    call-site built-in check should cache lookup - re-use caching + cleanup

--- core/src/main/java/org/jruby/runtime/callsite/CachingCallSite.java
+++ core/src/main/java/org/jruby/runtime/callsite/CachingCallSite.java
@@ -14,13 +14,11 @@ import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.builtin.IRubyObject;
 
 public abstract class CachingCallSite extends CallSite {
+
     protected CacheEntry cache = CacheEntry.NULL_CACHE;
-    //public static volatile int totalCallSites;
-    //private AtomicBoolean isPolymorphic = new AtomicBoolean(false);
 
     public CachingCallSite(String methodName, CallType callType) {
         super(methodName, callType);
-        //totalCallSites++;
     }
 
     public final CacheEntry getCache() {
@@ -301,18 +299,13 @@ public abstract class CachingCallSite extends CallSite {
         if (cache.typeOk(selfType)) {
             return cache.method.isBuiltin();
         }
-        return cacheAndGetBuiltin(selfType, methodName);
+        return cacheAndGet(selfType, methodName).method.isBuiltin(); // false for method.isUndefined()
     }
 
     private CacheEntry cacheAndGet(RubyClass selfType, String methodName) {
         CacheEntry entry = selfType.searchWithCache(methodName);
-        if (!entry.method.isUndefined()) cache = entry;
+        if (!entry.method.isUndefined()) this.cache = entry;
         return entry;
-    }
-
-    private static boolean cacheAndGetBuiltin(RubyClass selfType, String methodName) {
-        final DynamicMethod method = selfType.searchWithCache(methodName).method;
-        return method.isUndefined() && method.isBuiltin();
     }
     
     private IRubyObject cacheAndCall(IRubyObject caller, RubyClass selfType, Block block,
@@ -424,7 +417,7 @@ public abstract class CachingCallSite extends CallSite {
         cache = entry;
         return method.call(context, self, selfType, methodName, arg1, arg2, arg3, block);
     }
-
+    
     protected final IRubyObject callMethodMissing(ThreadContext context, IRubyObject self,
         DynamicMethod method, IRubyObject[] args) {
         return Helpers.selectMethodMissing(context, self, method.getVisibility(), methodName, callType).call(context, self, self.getMetaClass(), methodName, args, Block.NULL_BLOCK);
