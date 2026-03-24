923b5cede3e863431e72bd1316c5b9f6b66b5f10
Charles Oliver Nutter <headius@headius.com>
2026-03-23 22:36:14 -0500

    Make SeekableByteArayChannelImpl public

--- core/src/main/java/org/jruby/util/io/SeekableByteChannelImpl.java
+++ core/src/main/java/org/jruby/util/io/SeekableByteChannelImpl.java
@@ -24,7 +24,7 @@ import java.nio.channels.spi.AbstractInterruptibleChannel;
  * Seekable byte channel impl over a byte array stream.
  * @author kares
  */
-final class SeekableByteChannelImpl extends AbstractInterruptibleChannel
+public final class SeekableByteChannelImpl extends AbstractInterruptibleChannel
     implements ReadableByteChannel, SeekableByteChannel {
 
     private final ByteArrayInputStream in;
