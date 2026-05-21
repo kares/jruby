d53ac0607ae0d655ce5f08616dff43791dda1906
kares <self@kares.org>
2026-05-21 16:15:55 +0200

    [refactor] extract JCtorCache and cleanup

--- core/src/main/java/org/jruby/java/codegen/RealClassGenerator.java
+++ core/src/main/java/org/jruby/java/codegen/RealClassGenerator.java
@@ -56,7 +56,6 @@ import java.util.Map;
 import java.util.Set;
 
 import org.jruby.Ruby;
-import org.jruby.RubyArray;
 import org.jruby.RubyBasicObject;
 import org.jruby.RubyClass;
 import org.jruby.RubyClass.ConcreteJavaReifier;
@@ -69,7 +68,7 @@ import org.jruby.internal.runtime.methods.DynamicMethod;
 import org.jruby.java.proxies.ConcreteJavaProxy;
 import org.jruby.java.proxies.ConcreteJavaProxy.SplitCtorData;
 import org.jruby.java.proxies.MapJavaProxy;
-import org.jruby.javasupport.Java.JCtorCache;
+import org.jruby.javasupport.ConstructorCache;
 import org.jruby.javasupport.JavaConstructor;
 import org.jruby.javasupport.JavaUtil;
 import org.jruby.runtime.Block;
@@ -926,12 +925,12 @@ public abstract class RealClassGenerator {
         m.aload(rubyArrayIndex);
         m.aload(blockIndex); // load block from arg 3
         if (!hasParent) {
-            m.getstatic(cjr.javaPath, cjr.RUBY_CTOR_CACHE_FIELD, ci(JCtorCache.class));
+            m.getstatic(cjr.javaPath, cjr.RUBY_CTOR_CACHE_FIELD, ci(ConstructorCache.class));
         } else {
             m.aconst_null();
         }
         m.invokevirtual(cjr.rubyPath, "splitInitialized",
-                sig(SplitCtorData.class, RubyClass.class, IRubyObject[].class, Block.class, JCtorCache.class)); // pushes splitctordata
+                sig(SplitCtorData.class, RubyClass.class, IRubyObject[].class, Block.class, ConstructorCache.class)); // pushes splitctordata
 
         m.dup(); // splitctordata (results of splitInitialized)
 
